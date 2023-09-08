bf0905a70e8fd10de9401b9316a05f1300f87858
rysk-17 <136770680+rysk-17@users.noreply.github.com>
2023-09-08 10:42:40 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,11 @@
+$(function() {
+    // スクロールされた場合
+    $(document).on({
+        'load' : () => {
+            console.log('loadイベントが発生しました')
+        },
+        'scroll' : () => {
+            console.log('scrollイベントが発生しました')
+        },
+    });
+  });
